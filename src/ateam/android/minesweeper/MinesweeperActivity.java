e3c19d0b4c6887ea7e63e278d4183f28adce4be6
m3dteam <m3dtem@hotmail.com>
2012-01-31 20:18:44 -0500

    first "hello world" app

---
+++ src/ateam/android/minesweeper/MinesweeperActivity.java
@@ -0,0 +1,19 @@
+package ateam.android.minesweeper;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.os.Bundle;
+
+public class MinesweeperActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView  tv = new TextView(this);
+        
+        tv.setText( "Hi There");
+        setContentView(tv);
+    }
+}
